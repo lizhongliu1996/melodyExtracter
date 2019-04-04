ad4fb251ea319fef36c823628a960e8335e10fa3
Lizhong Liu <lxl816@miami.edu>
2019-04-04 01:25:16 -0400

    add steps

--- R/melodyExtractor.R
+++ R/melodyExtractor.R
@@ -1,4 +1,24 @@
 ### this function that remove the human vocal part melody
-melodyExtractor <- function(){
+melodyExtractor <- function(
+                          songinfile,
+                          songoutfile,
+                          compensate = c(TRUE, FALSE)
+                          ){
+
+  ## 1. first load song
+
+
+  ## 2. divided into left mono and right mono
+
+
+  ## 3. combine left and right by left - right
+
+
+  ## 4. stereo the combined audio
+
+  ## 5. add eq low and high pass
+
+  ## 6. output audio file
+
 
 }
